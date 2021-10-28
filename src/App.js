fb7b555e6e3584172bc0187ca5e7e66623a38319
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-10-28 16:13:57 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+export default function App () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>Hello World </h1>
+        <p> This is my Dictionary Project</p>
+
+      </header>
+    </div>
+  );
+}
+
+
