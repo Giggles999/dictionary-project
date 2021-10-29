14f57bbad944fe54d7d3ce98c33f9bfb295ffc73
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-10-28 17:18:07 -0700

    Created Search Form & Fix Button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
+import Dictionary from "./Dictionary"
 
 export default function App () {
   return (
     <div className="App">
+     <div className="container">
       <header className="App-header">
         
         <h1>Hello World </h1>
         <p> This is my Dictionary Project</p>
 
-      <a href="#" className="btn btn-primary shadow">Submit</a>
+      <button className="btn btn-primary shadow">Submit</button>
       </header>
+    <main>
+      <Dictionary/>
+      </main>
+    
+      <footer className="App-footer">
+      <small> Coded by Lynn Mach</small>
+      </footer>
     </div>
+</div>
   );
 }
 
