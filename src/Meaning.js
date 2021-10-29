ab9703b35889a468f1f1e0cc07b9e9b88e561592
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-10-28 20:28:55 -0700

    Created Results and Pulled in Meanings

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,16 +4,12 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h4>{props.meaning.partOfSpeech}</h4>
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <br />
-              <em>{definition.example}</em>
-            </p>
+            <p>{definition.definition}<br/>
+            <em>{definition.example}</em></p>
           </div>
         );
       })}
